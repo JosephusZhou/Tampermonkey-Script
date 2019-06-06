0e0fe3196bb8eff2d64d54a024eb66ef8a141d08
zhousenfeng <senfeng.zhou@bcsystech.com>
2019-06-06 17:15:03 +0800

    带密码跳转

--- js/ToDuPan.js
+++ js/ToDuPan.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         To DuPan
 // @namespace    https://github.com/JosephusZhou
-// @version      0.1
+// @version      0.2
 // @description  Automatically redirect to DuPan.
 // @author       Josephus Zhou
 // @match        https://pan.baidu.com/s/*
+// @match        https://pan.baidu.com/share/*
 // @grant        none
 // ==/UserScript==
 
@@ -12,9 +13,18 @@
 	'use strict';
 
 	// 1 -> 2
-	// https://pan.baidu.com/s/1bt2DcoDPEAZ1qJZ2x2PkFw
+	// https://pan.baidu.com/s/1bt2DcoDPEAZ1qJZ2x2PkFw or https://pan.baidu.com/share/init?surl=bt2DcoDPEAZ1qJZ2x2PkFw
 	// https://pan.baiduwp.com/s/1bt2DcoDPEAZ1qJZ2x2PkFw
 	let originalUrl = window.location.href
-	let jumpUrl = originalUrl.replace("baidu.com", "baiduwp.com")
+	let jumpUrl
+	let reg = /https\:\/\/pan\.baidu\.com\/share\/init\?surl=(.*)/
+	if (reg.test(originalUrl)) {
+		// 带密码解析后跳转
+		var regResult = originalUrl.match(reg)
+		jumpUrl = "https://pan.baiduwp.com/s/1" + regResult[1]
+	} else {
+		// 无密码直接跳转
+		jumpUrl = originalUrl.replace("baidu.com", "baiduwp.com")
+	}
 	window.location.replace(jumpUrl)
 })();
